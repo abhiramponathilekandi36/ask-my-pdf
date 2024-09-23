e74b40fe0250b2419f62abc5983409262377c36b
Abhiram Ponathile Kandi <abhiramponathilekandi36@gmail.com>
2024-09-23 20:22:34 +0530

    fix build error

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -10,7 +10,7 @@ interface PageProps {
   };
 }
 
-export const metdata = {
+export const metadata = {
   title: "Chat",
 };
 
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-export const metdata = {
+export const metadata = {
   title: "Dashboard",
 };
 
