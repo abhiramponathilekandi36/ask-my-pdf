726879f9b05ecb93805c2c78c3b6091a8999153c
abhiramponathilekandi36 <abhiramponathilekandi36@gmail.com>
2024-09-24 10:09:03 +0530

    Update UploadButton.tsx

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -87,12 +87,12 @@ const UploadDropzone = () => {
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
-          className="hidden md:block border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+          className=" border h-64 m-4 border-dashed border-gray-300 rounded-lg"
         >
           <div className="flex items-center justify-center h-full w-full">
             <label
               htmlFor="dropzone-file"
-              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+              className=" hidden md:flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
             >
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
